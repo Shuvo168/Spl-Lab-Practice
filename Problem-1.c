6b964a8cbfa2d251bde773b615d0550badaff139
Shuvo168 <mdsayhamhossainshuvo@gmail.com>
2025-02-24 17:33:55 +0600

    add

--- Problem-1.c
+++ Problem-1.c
@@ -1,6 +1,25 @@
+// Write a C program to find the area and perimeter of a triangle.
+
 #include <stdio.h>
+#include <math.h>
 int main()
 {
+    int a, b, c, perimeter, semiperimeter, area;
+    printf("Enter the three sides of the triangle: ");
+    scanf("%d %d %d", &a, &b, &c);
 
-return 0;
+    if (a + b > c && b + c > a && c + a > b)
+    {
+        perimeter = a + b + c;
+        semiperimeter = perimeter / 2;
+        area = sqrt(semiperimeter * (semiperimeter - a) * (semiperimeter - b) * (semiperimeter - c));
+        printf("Perimeter of the triangle: %d \n", perimeter);
+        printf("Area of the triangle: %d", area);
+    }
+    else
+    {
+        printf("Invalid triangle!");
+    }
+
+    return 0;
 }
